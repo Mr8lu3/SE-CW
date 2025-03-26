ebe3cd2a02045eab2202f6c253b67e47b8d51f59
leospx <leonidspirkoski2003@gmail.com>
2025-03-26 19:49:45 +0000

    controllerpages

---
+++ usercontroller.js
@@ -0,0 +1,20 @@
+// controllers/userController.js
+// Example of a user controller to render user profile page
+const userModel = require('../models/userModel'); // Import the user model
+
+async function getUserProfile(req, res) {
+  try {
+    const userId = req.params.userId; // Assume you're passing a user ID
+    const userProfile = await userModel.getUserById(userId);
+
+    // Send user profile data to the view (e.g., profile.pug)
+    res.render('userpage', { user: userProfile });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+module.exports = {
+  getUserProfile
+};
